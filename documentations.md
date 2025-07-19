32c6f4fa804ac1bcb9161d92e7b5e7d7979c060e
Jun Luzon <ldiary@users.noreply.github.com>
2025-07-19 09:15:03 +0100

    Injector working. No need for cucumber.properties anymore. The injector needs to placed inside the glue directory.

--- documentations.md
+++ documentations.md
@@ -1,1 +1,3 @@
 https://cucumber.io/docs/cucumber/api/
+
+https://github.com/cucumber/cucumber-jvm/tree/main/cucumber-guice
--- src/test/java/steps/GuiceInjector.java
+++ src/test/java/steps/GuiceInjector.java
@@ -1,13 +1,14 @@
-package config.inject;
+package steps;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 import config.Environment;
+import config.inject.Platform;
 import io.cucumber.guice.CucumberModules;
 import io.cucumber.guice.InjectorSource;
 
-public class CucumberGuiceInjector implements InjectorSource {
+public class GuiceInjector implements InjectorSource {
 
     private final String platform = Environment.requiredSystemProperty("platform");
 
